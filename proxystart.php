cdf6a140e1d3bbe5d9188a24a7712291f7b6dca3
guoxinhua <guoxinhua@10.10.12.142>
2017-07-26 11:10:30 +0800

    删除so文件

--- proxystart.php
+++ proxystart.php
@@ -3,6 +3,6 @@ define('ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR);
 require ROOT_PATH . '/Vendor/Autoloader.php';
 \Vendor\Autoloader::instance()->addRoot(ROOT_PATH)->addRoot(ROOT_PATH . '/Vendor/')->init();
 
-$core = new \Core\MysqlProxy();
+$core = new \Proxy\MysqlProxy();
 $core->init();
 $core->start();
